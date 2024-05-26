3cb56248ea673a6c96354a0ac9be5371f5371d22
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-05-25 21:12:07 -0400

    0.0.22

--- behance.js
+++ behance.js
@@ -1,4 +1,5 @@
 import { BeClonable } from './be-clonable.js';
+export { BeClonable } from './be-clonable.js';
 import { def } from 'trans-render/lib/def.js';
 await BeClonable.bootUp();
 def('be-clonable', BeClonable);
--- behivior.js
+++ behivior.js
@@ -1,17 +1,18 @@
-import './behance.js';
 import { BeHive } from 'be-hive/be-hive.js';
-BeHive.registry.register({
-    base: 'be-clonable',
-    enhPropKey: 'beClonable',
+import { MountObserver } from 'mount-observer/MountObserver.js';
+const base = 'be-clonable';
+const emc = {
+    base,
     map: {
         '0.0': 'ni'
     },
-    do: {
-        mount: {
-            import: async () => {
-                const { BeClonable } = await import('./be-clonable.js');
-                return BeClonable;
-            }
-        }
+    enhPropKey: 'beClonable',
+    importEnh: async () => {
+        const { BeClonable } = await import('./behance.js');
+        return BeClonable;
     }
-});
+};
+const mose = document.createElement('script');
+mose.id = base;
+mose.synConfig = emc;
+MountObserver.synthesize(document, BeHive, mose);
