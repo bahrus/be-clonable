46f217136bd0d1fbc698813930635dffdc2c3624
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-04-09 21:57:39 -0400

    0.0.26

---
+++ emc.mjs
@@ -0,0 +1,44 @@
+//@ts-check
+
+/** @import {EMC} from './types/mount-observer/types' */;
+/** @import {AllProps, Actions} from './types/be-clonable/types' */
+/** @import {RAConfig} from './types/roundabout/types' */
+
+/**
+ * @type {EMC<any, AllProps, Element, RAConfig<AllProps, Actions> >}
+ */
+export const emc = {
+    enhConfig: {
+        enhKey: 'BeClonable',
+        spawn: 'be-clonable/be-clonable.js',
+        withAttrs: {
+            base: 'be-clonable',
+            triggerInsertPosition: '${base}-trigger-insert-position',
+            cloneInsertPosition: '${base}-clone-insert-position',
+            buttonContent: '${base}-button-content',
+        }
+    },
+    customData: {
+        actions: {
+            addCloneBtn: {
+                ifAllOf: ['triggerInsertPosition'],
+            },
+            setBtnContent: {
+                ifAllOf: ['buttonContent', 'trigger'],
+                ifNoneOf: ['byob'],
+            }
+        },
+        handlers: {
+            trigger_to_beCloned_on: 'click'
+        },
+        compacts:{
+            when_resolved_changes_dispatch: 'resolved',
+        }
+    }
+}
+
+export function render(){
+    return JSON.stringify(emc, null, 4);
+}
+
+console.log(render());
